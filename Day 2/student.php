3cec1965a85c528e68ea38c527fb6f4b62eb24ab
KunKimlong <kimlongkun2412@gmail.com>
2025-01-02 22:15:25 +0700

    Day 2

---
+++ Day 2/student.php
@@ -0,0 +1,9 @@
+<?php 
+    #Retrive student data from file studentForm
+    #Retrive id, name, gender, score1, score2, score3, total, average
+
+    # $total = $score1+...+$score3
+    # $avg = $total/3;
+    
+    #show as Table
+?>
